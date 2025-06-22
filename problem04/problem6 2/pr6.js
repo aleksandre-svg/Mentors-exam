57a23ac93324852741f7049169565db710b1aaa0
Diana044 <dzukaevadiana3@gmail.com>
2025-06-22 11:50:25 +0400

    commit

---
+++ problem04/problem6 2/pr6.js
@@ -0,0 +1,12 @@
+const numSum = (arg1) => {
+    let numbers = '0123456789'
+    let sum = 0
+    for (let i of String(arg1)){
+        if (numbers.includes(i)){
+            sum+=Number(i)
+        }
+    }
+    return sum
+}
+
+console.log(numSum("1a2b3c"))
